8ecf9e89a6f02995eb1657219f8ce0f636b6fcb6
konnnnitiha84238 <pawahara@pwpwa.com>
2025-06-30 17:15:50 +0900

    Update geography.js

--- public/contact/geography.js
+++ public/contact/geography.js
@@ -1,10 +1,38 @@
-self.__uv$config = {
-    prefix: '/reviews/',
-    bare: '/outerspace/',
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: 'https://raw.githubusercontent.com/Tenakskd/kasuguma/refs/heads/master/static/contact/algebra.js',
-    bundle: 'https://raw.githubusercontent.com/Tenakskd/kasuguma/refs/heads/master/static/contact/mathematics.js',
-    config: '/contact/geography.js',
-    sw: 'https://raw.githubusercontent.com/Tenakskd/kasuguma/refs/heads/master/static/contact/english.js'
-};
+function _0x3e11() {
+    var _0x30fb0b = ['/contact/geography.js', '/outerspace/', 'decode', '23620CMpVSx', '10ZnLJqY', 'https://raw.githubusercontent.com/Tenakskd/kasuguma/refs/heads/master/static/contact/english.js', 'xor', '1060RcCtPS', '220734wgUCim', '40190vbXRmu', '90893fqUxcP', 'https://raw.githubusercontent.com/Tenakskd/kasuguma/refs/heads/master/static/contact/mathematics.js', 'codec', 'encode', '2001258XuelFK', '546948HQSmqQ', '32zElkGB', '888180WjpZuh', '264JosIdb'];
+    _0x3e11 = function() {
+        return _0x30fb0b;
+    };
+    return _0x3e11();
+}
+var _0x303d18 = _0x2230;
+
+function _0x2230(_0x2718e2, _0x17e3d0) {
+    var _0x3e11ff = _0x3e11();
+    return _0x2230 = function(_0x2230da, _0x486c36) {
+        _0x2230da = _0x2230da - 0x1ba;
+        var _0x177d99 = _0x3e11ff[_0x2230da];
+        return _0x177d99;
+    }, _0x2230(_0x2718e2, _0x17e3d0);
+}(function(_0x1f532f, _0x33b1f3) {
+    var _0x377d98 = _0x2230,
+        _0x3d3890 = _0x1f532f();
+    while (!![]) {
+        try {
+            var _0x281998 = parseInt(_0x377d98(0x1bf)) / 0x1 * (parseInt(_0x377d98(0x1ba)) / 0x2) + parseInt(_0x377d98(0x1be)) / 0x3 + -parseInt(_0x377d98(0x1c8)) / 0x4 * (parseInt(_0x377d98(0x1cc)) / 0x5) + -parseInt(_0x377d98(0x1c7)) / 0x6 + -parseInt(_0x377d98(0x1c4)) / 0x7 + -parseInt(_0x377d98(0x1c6)) / 0x8 * (parseInt(_0x377d98(0x1c5)) / 0x9) + parseInt(_0x377d98(0x1bd)) / 0xa * (parseInt(_0x377d98(0x1c0)) / 0xb);
+            if (_0x281998 === _0x33b1f3) break;
+            else _0x3d3890['push'](_0x3d3890['shift']());
+        } catch (_0x1406fc) {
+            _0x3d3890['push'](_0x3d3890['shift']());
+        }
+    }
+}(_0x3e11, 0x2774a), self['__uv$config'] = {
+    'prefix': '/reviews/',
+    'bare': _0x303d18(0x1ca),
+    'encodeUrl': Ultraviolet['codec'][_0x303d18(0x1bc)][_0x303d18(0x1c3)],
+    'decodeUrl': Ultraviolet[_0x303d18(0x1c2)][_0x303d18(0x1bc)][_0x303d18(0x1cb)],
+    'handler': 'https://raw.githubusercontent.com/Tenakskd/kasuguma/refs/heads/master/static/contact/algebra.js',
+    'bundle': _0x303d18(0x1c1),
+    'config': _0x303d18(0x1c9),
+    'sw': _0x303d18(0x1bb)
+});
