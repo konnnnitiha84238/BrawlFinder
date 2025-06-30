0a42cf890798af53763c70f917675ac9bac98305
konnnnitiha84238 <pawahara@pwpwa.com>
2025-06-30 20:32:26 +0900

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express(server);
 const bareServer = createBareServer('/outerspace/');
 const PORT = 8080;
 const limit = process.env.LIMIT || 50;
-const user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.3.1 Safari/605.1.15";
+const user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Safari/605.1.15";
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
