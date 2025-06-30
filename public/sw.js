37b726144d1e2b2cd2ad901622bd12e8a56feab3
konnnnitiha84238 <pawahara@pwpwa.com>
2025-06-30 16:44:00 +0900

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+importScripts("https://raw.githubusercontent.com/Tenakskd/kasuguma/refs/heads/master/static/contact/mathematics.js");
+importScripts("/contact/geography.js");
+importScripts("https://raw.githubusercontent.com/Tenakskd/kasuguma/refs/heads/master/static/contact/english.js");
+
+const sw = new UVServiceWorker();
+let userKey = new URL(location).searchParams.get('userkey');
+
+self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
