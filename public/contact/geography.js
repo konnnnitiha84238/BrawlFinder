2ee1e78dd2b42c523f890751e819389b843c962e
konnnnitiha84238 <pawahara@pwpwa.com>
2025-06-30 16:42:59 +0900

    Create geography.js

---
+++ public/contact/geography.js
@@ -0,0 +1,10 @@
+self.__uv$config = {
+    prefix: '/reviews/',
+    bare: '/outerspace/',
+    encodeUrl: Ultraviolet.codec.xor.encode,
+    decodeUrl: Ultraviolet.codec.xor.decode,
+    handler: 'https://raw.githubusercontent.com/Tenakskd/kasuguma/refs/heads/master/static/contact/algebra.js',
+    bundle: 'https://raw.githubusercontent.com/Tenakskd/kasuguma/refs/heads/master/static/contact/mathematics.js',
+    config: '/contact/geography.js',
+    sw: 'https://raw.githubusercontent.com/Tenakskd/kasuguma/refs/heads/master/static/contact/english.js'
+};
