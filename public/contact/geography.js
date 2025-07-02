3d7e2694c85342cf81247d4264bd3f55b9b2d25b
konnnnitiha84238 <pawahara@pwpwa.com>
2025-07-02 20:46:47 +0900

    Update geography.js

--- public/contact/geography.js
+++ public/contact/geography.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
-    prefix: '/bare/',
-    bare: '/outerspace/',
+    prefix: '/reviews/',
+    bare: '/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/contact/algebra.js',
